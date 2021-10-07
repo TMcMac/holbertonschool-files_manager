0d8c2a12d141be22210db751af869602bbb6ca4a
Cort Robinson <cortrobinson99@gmail.com>
2021-10-07 12:06:53 -0500

    add code to utils/redis.js

---
+++ utils/redis.js
@@ -0,0 +1,27 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (err) => console.error(`Redis client not connected to the server: ${err.message}`));
+  }
+
+  isAlive() { return this.client.connected; }
+
+  async get(key) {
+    const value = await promisify(this.client.get).bind(this.client)(key);
+    return value;
+  }
+
+  async set(key, value, duration) {
+    await promisify(this.client.set).bind(this.client)(key, value, 'EX', duration);
+  }
+
+  async del(key) {
+    await promisify(this.client.del).bind(this.client)(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
