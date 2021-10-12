123f815d2408dc602cd552415a6c2d46a6cb77cd
Cort Robinson <cortrobinson99@gmail.com>
2021-10-12 15:09:49 -0500

    update to UsersController

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,3 +1,4 @@
+import crypto from 'crypto';
 import dbClient from '../utils/db';
 
 class UsersController {
@@ -5,6 +6,16 @@ class UsersController {
     const { email, password } = req.body;
     if (!email) return res.status(400).json({ error: 'Missing email' });
     if (!password) return res.status(400).json({ error: 'Missing password' });
+
+    const users = await dbClient.db.collection('users');
+
+    const foundUserArray = await users.find({ email }).toArray();
+    if (foundUserArray.length > 0) return res.status(400).json({ error: 'Already exist' });
+
+    const hashedPass = crypto.createHash('SHA1').update(password).digest('hex');
+    const userCreationObj = await users.insertOne({ email, password: hashedPass });
+    const newUser = { id: userCreationObj.insertedId, email };
+    return res.status(201).json(newUser);
   }
 }
 
