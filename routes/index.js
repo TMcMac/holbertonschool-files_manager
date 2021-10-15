fbea66f911c763fcab8a701e7bbe19919574c523
Jilroge7 <1672@holbertonschool.com>
2021-10-15 04:12:35 +0000

    forgot FileController to .putPublish/Unpublish..

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,6 @@ router.get('/users/me', UsersController.getMe);
 router.post('/files', FilesController.postUpload);
 router.get('/files/:id', FilesController.getShow);
 router.get('/files/:id', FilesController.getIndex);
-router.put('/files/:id/publish', putPublish);
-router.put('/files/:id/unpublish', putUnpublish);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
 export default router;
